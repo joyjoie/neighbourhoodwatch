0afb33476b29d4d8c73dfc59cdb34aa4c31dfbac
joy <wendokingoo@gmail.com>
2019-03-23 22:54:26 +0300

    First model class created and media files settings updated

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to="image/")
+    caption = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -4,3 +4,5 @@ from . import views
 urlpatterns=[
     url('^$',views.index,name = 'index'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
