a8c155fa08599f84cb8eaa86c2f7422e2ffb4544
joy <wendokingoo@gmail.com>
2019-03-24 15:12:12 +0300

    Profile views added

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -6,3 +6,11 @@ from .models import Neighbourhood,Business,User
 def index(request):
     images = Neighbourhood.display_neighbourhood()
     return render(request,'photos/index.html' , {"images":images})
+
+
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    profile = request.user.profile   
+    # images = Project.objects.all().filter(id = profile.user.id)
+   
+    return render(request, 'profile/profile.html', { "profile":profile})
