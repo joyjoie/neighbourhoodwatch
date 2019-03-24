d7638446263bac47516fd8621b12c80500c6dcf9
joy <wendokingoo@gmail.com>
2019-03-24 15:38:19 +0300

    Profile class created in models file

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -42,4 +45,44 @@ class Business(models.Model):
         Neighbourhood.objects.all().delete()
     
     def update(self): 
-       Neighbourhood.objects.filter(id = 2).update(name ='Kim')
+       Neighbourhood.objects.filter(id = 2).update()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.CharField(max_length=60 ,blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+       self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self, bio):
+        self.bio = bio
+        self.save()
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    @classmethod
+    def pro(cls):
+        return cls.objects.all()
+
+ 
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
