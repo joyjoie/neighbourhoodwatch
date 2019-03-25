4739807816c952e9ef3111d7a24d563196e5ccf3
joy <wendokingoo@gmail.com>
2019-03-25 20:53:22 +0300

    Test added and modified

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,3 +1,72 @@
 from django.test import TestCase
-
+import datetime as dt
 # Create your tests here.
+from .models import Neighbourhood,Profile,Business,Comments
+import datetime as dt
+
+class NeighbourhoodTestClass(TestCase):
+     # Set up method
+    def setUp(self):
+        self.type= Neighbourhood(name='south')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.type,Neighbourhood))
+
+    def test_init(self):
+       
+        self.assertTrue(self.type.name == 'south')
+
+  
+    def tearDown(self):
+       Neighbourhood.objects.all().delete()
+       
+
+class ProfileTestClass(TestCase):
+     # Set up method
+    def setUp(self):
+        self.type= Profile(bio ="me")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.type,Profile))
+
+    def test_init(self):
+       
+        self.assertTrue(self.type.bio == "me")
+
+  
+    def tearDown(self):
+       Profile.objects.all().delete()
+    
+
+class BusinessTestClass(TestCase):
+     # Set up method
+    def setUp(self):
+        self.type= Business(name ="joy")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.type,Business))
+
+    def test_init(self):
+       
+        self.assertTrue(self.type.name == "joy")
+
+  
+    def tearDown(self):
+       Business.objects.all().delete()
+
+
+class CommentsTestClass(TestCase):
+     # Set up method
+    def setUp(self):
+        self.type= Comments(comment="hi")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.type,Comments))
+
+    def test_init(self):
+       
+        self.assertTrue(self.type.comment == "hi")
+
+  
+    def tearDown(self):
+       Comments.objects.all().delete()
